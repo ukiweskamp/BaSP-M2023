69dfd8ec9500abc2588682d2cd7e371f001ff1b7
Eugenio Weskamp <e.weskamp8@gmail.com>
2023-04-23 17:02:50 -0300

    BaSP-W06-2023: add login.js logic

---
+++ Week-06/js/sign-up.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+console.log('hola')
+
+// VARIABLES
+
+var nameInput = document.getElementById('name');
+var lastName = document.getElementById('lastname');
+var id = document.getElementById('id')
+var birthday = document.getElementById('birthday');
+var phone = document.getElementById('phone');
+var address = document.getElementById('address');
+var locationInput = document.getElementById('location');
+var zipCode = document.getElementById('zipcode');
+var password = document.getElementById('password');
+var repeatPassword = document.getElementById('repeat-password');
+
+// NAME
+
+
+nameInput.addEventListener('blur',function(){
+    var 
+})
+
+
+
+
+
+
+}
